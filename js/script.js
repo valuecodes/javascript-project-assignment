a0a15e11212ca6068d5ddb989d9f8fd31fe053f3
Tietohallinto <tietohallinto@BC-minimac053.local>
2020-10-02 12:12:32 +0300

    Added navigationCircle smooth movement

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
 class NavigationCircle{
     constructor(el,size){
         this.el = el
-        this.xPos = 70
-        this.yPos = 105
+        this.yPos = 70
+        this.xPos = 105
         this.size = size
         this.offset = size/2
+        
         document.body.addEventListener('click',(e)=>this.move(e))
     }
     move(e){
-        const xTarget = e.clientX
-        const yTarget = e.clientY
-        this.xPos = xTarget
-        this.yPos = yTarget
-        const offset = this.offset
-        this.el.style.left = xTarget-offset+'px'
-        this.el.style.top = yTarget-offset+'px'
+        const xTarget = e.clientX-this.offset
+        const yTarget = e.clientY-this.offset
+        let currentX = this.xPos
+        let currentY = this.yPos
+        const xSpeed = Math.abs((xTarget-currentX)/100)
+        const ySpeed = Math.abs((yTarget-currentY)/100)
+        
+        function moveElem(elem){
+            if(currentX!==xTarget){
+                currentX +=  currentX<xTarget?xSpeed:-xSpeed
+            }
+            if(currentX!==xTarget){
+                currentY +=  currentY<yTarget?ySpeed:-ySpeed
+            }       
+            elem.xPos = currentX
+            elem.yPos = currentY
+            elem.el.style.left = currentX+'px'
+            elem.el.style.top = currentY+'px'   
+            if(Math.abs(currentX-xTarget)>1&&Math.abs(currentY-yTarget)>1){
+                setTimeout(()=>{
+                    moveElem(elem)
+                },10)
+            }              
+        }
+        moveElem(this)
     }
 
 }
