a0e32654eb57be9557d1dbb9f588275c84efe890
Tietohallinto <tietohallinto@BC-minimac053.local>
2020-10-02 14:40:02 +0300

    Added obstacles and obstacle detection

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 class NavigationCircle{
-    constructor(el,size,infoContainers){
+    constructor(el,size,infoContainers,obstacles){
         this.el = el
         this.yPos = 70
         this.xPos = 105
         this.size = size
         this.offset = size/2
         this.infoContainers = infoContainers
+        this.obstacles = obstacles
         document.body.addEventListener('click',(e)=>this.move(e))
     }
     move(e){
@@ -30,7 +31,15 @@ class NavigationCircle{
                  infoContainer.checkIfCircleIsInside(elem)   
             )
             
-            if(Math.abs(currentX-xTarget)>1&&Math.abs(currentY-yTarget)>1){
+            let collision = false;
+
+            elem.obstacles.forEach(obstacle =>{
+                if(obstacle.detectCollision(elem)){
+                    collision=true
+                } 
+            })
+
+            if(Math.abs(currentX-xTarget)>1&&Math.abs(currentY-yTarget)>1&&!collision){
                 setTimeout(()=>{
                     moveElem(elem)
                 },10)
@@ -41,7 +50,7 @@ class NavigationCircle{
 }
 
 class InfoContainer{
-    constructor(elem,dimensions){
+    constructor(elem){
         this.elem = elem
         this.bgcolor = ''
     }
@@ -57,18 +66,39 @@ class InfoContainer{
     }
 }
 
+class Obstacle{
+    constructor(elem){
+        this.elem = elem
+    }
+    detectCollision(navCircle){
+        const { yPos, xPos } = navCircle
+        const { top, right, bottom, left } = this.elem.getBoundingClientRect()
+        if(yPos>top&&yPos<bottom&&xPos>left&&xPos<right){
+            this.elem.style.backgroundColor = 'salmon'
+            return true
+        }else{
+            this.elem.style.backgroundColor = 'dimgray'
+            return false
+        }
+    }
+}
+
 (function() {
 
     let infoContainers = []
+    let obstacles = []
 
     for(var i=1;i<=4;i++){
         const infoContainerElement = document.getElementById('infoContainer'+i)   
         infoContainers.push(new InfoContainer(infoContainerElement))     
     }
 
-    console.log(window.innerHeight)
+    for(var i=1;i<=2;i++){
+        const obstacleElement = document.getElementById('obstacle'+i)   
+        obstacles.push(new Obstacle(obstacleElement))
+    }
 
     const navElement = document.getElementById('navigationCircle')
-    const navCircle = new NavigationCircle(navElement,30,infoContainers)
+    const navCircle = new NavigationCircle(navElement,30,infoContainers,obstacles)
  
 })();
