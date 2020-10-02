8d0ac1f490aab4593a7c6449119ae3ef515bf58f
Tietohallinto <tietohallinto@BC-minimac053.local>
2020-10-02 10:45:06 +0300

    Added NavigationCircle class and click event movement function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+class NavigationCircle{
+    constructor(el,size){
+        this.el = el
+        this.xPos = 70
+        this.yPos = 105
+        this.size = size
+        this.offset = size/2
+        document.body.addEventListener('click',(e)=>this.move(e))
+    }
+    move(e){
+        const xTarget = e.clientX
+        const yTarget = e.clientY
+        this.xPos = xTarget
+        this.yPos = yTarget
+        const offset = this.offset
+        this.el.style.left = xTarget-offset+'px'
+        this.el.style.top = yTarget-offset+'px'
+    }
 
+}
+
+(function() {
+    const navElement = document.getElementById('navigationCircle')
+    const navCircle = new NavigationCircle(navElement,30)
+
+ })();
