449ed4f746f955b721905e45974edabc69ea8032
valuecodes <juhakangas5@gmail.com>
2020-10-09 12:24:08 +0300

    Added bird speaking function and rotation towards moving direction

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 class NavigationCircle{
     constructor(el,size,infoContainers,obstacles){
         this.el = el
+        this.speakingBubble = document.getElementById('speakingBubble')
         this.yPos = 70
         this.xPos = 105
         this.size = size
@@ -10,12 +11,17 @@ class NavigationCircle{
         document.body.addEventListener('click',(e)=>this.move(e))
     }
     move(e){
+        this.speakingBubble.style.visibility = 'hidden'
+        
         const xTarget = e.clientX-this.offset
         const yTarget = e.clientY-this.offset
         let currentX = this.xPos
         let currentY = this.yPos
         const xSpeed = Math.abs((xTarget-currentX)/100)
         const ySpeed = Math.abs((yTarget-currentY)/100)
+
+        this.rotate(xTarget,yTarget,currentX,currentY)
+        
         function moveElem(elem){
             if(currentX!==xTarget){
                 currentX +=  currentX<xTarget?xSpeed:-xSpeed
@@ -27,6 +33,7 @@ class NavigationCircle{
             elem.yPos = currentY
             elem.el.style.left = currentX+'px'
             elem.el.style.top = currentY+'px'
+
             elem.infoContainers.forEach(infoContainer =>
                  infoContainer.checkIfCircleIsInside(elem)   
             )
@@ -47,18 +54,38 @@ class NavigationCircle{
         }
         moveElem(this)
     }
+    speak(line,fontSize=10){
+        this.speakingBubble.style.fontSize = fontSize+'px'
+        this.speakingBubble.style.visibility = 'visible'
+        this.speakingBubble.textContent = line
+    }
+    rotate(xTarget,yTarget,currentX,currentY){
+        let rotation = 0
+        if(xTarget<currentX){
+            rotation-=180
+        }
+        this.speakingBubble.style.transform = `rotateY(${rotation}deg)`
+        this.el.style.transform = `rotateY(${rotation}deg)`;
+    }
 }
 
 class InfoContainer{
-    constructor(elem){
+    constructor(elem,index){
         this.elem = elem
         this.bgcolor = ''
+        this.index = index
     }
 
     checkIfCircleIsInside(navCircle){
         const { yPos, xPos } = navCircle
+        const { index } = this
         const { top, right, bottom, left } = this.elem.getBoundingClientRect()
+        console.log(index)
         if(yPos>top&&yPos<bottom&&xPos>left&&xPos<right){
+            if(index===1)navCircle.speak('Hey I found something!')
+            if(index===2)navCircle.speak('Nice picture')
+            if(index===3)navCircle.speak('Click download to get pdf')
+            if(index===4)navCircle.speak('Destination reached!')
             this.elem.style.cssText = 'filter: blur(0);'
         }else{
             this.elem.style.cssText = 'filter: blur(50px);'
@@ -71,9 +98,15 @@ class Obstacle{
         this.elem = elem
     }
     detectCollision(navCircle){
-        const { yPos, xPos } = navCircle
+        const { yPos, xPos, size } = navCircle
         const { top, right, bottom, left } = this.elem.getBoundingClientRect()
-        if(yPos>top&&yPos<bottom&&xPos>left&&xPos<right){
+        if(
+            yPos+size>top&&
+            yPos<bottom&&
+            xPos+size>left&&
+            xPos<right
+        ){
+            navCircle.speak('Ouch I hit a wall!')
             this.elem.style.backgroundColor = 'salmon'
             return true
         }else{
@@ -90,7 +123,7 @@ class Obstacle{
 
     for(var i=1;i<=4;i++){
         const infoContainerElement = document.getElementById('infoContainer'+i)   
-        infoContainers.push(new InfoContainer(infoContainerElement))     
+        infoContainers.push(new InfoContainer(infoContainerElement,i))     
     }
 
     for(var i=1;i<=6;i++){
